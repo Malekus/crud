a96d5223fdf419535b4450792c12026f501deb3c
Malekus <lereglissedu92@hotmail.fr>
2020-03-06 07:44:53 +0100

    make datatable

--- app/Http/Controllers/RapportController.php
+++ app/Http/Controllers/RapportController.php
@@ -14,13 +14,14 @@ class RapportController extends Controller
         $etablissements = Etablissement::all()->pluck('full_name', 'id');
         $educateurs = Educateur::all()->pluck('full_name', 'id');
         $eleves = Eleve::all()->take(15);
-        return view('rapport.index', compact(['eleves', 'etablissements', 'educateurs']));
+        return view('rapport.index', compact('eleves'));
+        //return view('rapport.index', compact(['eleves', 'etablissements', 'educateurs']));
     }
 
     public function exportPDF()
     {
         $eleve = Eleve::find(1);
-        
+
         return view('rapport.exportPDF', compact(['eleve']));
         $pdf = PDF::loadView('rapport.exportPDF', compact(['eleve']));
         return $pdf->stream();
--- resources/views/rapport/index.blade.php
+++ resources/views/rapport/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layout.base')
 
+@section('endStyle')
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-12">
@@ -23,92 +29,35 @@
                                 {!! Form::close() !!}
                             </div>
                             <div class="float-left">
-                                <a href="{{ route("rapport.exportPDF") }}" target="_blank" id="btnExportPDF" class="btn btn-success mx-sm-3 mb-2">
+                                <a href="{{ route("rapport.exportPDF") }}" target="_blank" id="btnExportPDF"
+                                   class="btn btn-success mx-sm-3 mb-2">
                                     Exporter PDF
                                 </a>
                             </div>
                         </div>
                     </div>
-
-                    <div class="row">
-                        <div class="col-12 mt-2">
-                            <div class="table-responsive">
-                                <table class="table table-bordered table-hover">
-                                    <thead>
-                                    <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">Nom</th>
-                                        <th scope="col">Prénom</th>
-                                        <th scope="col">Dernière activité</th>
-                                        <th scope="col">Action</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @foreach($eleves as $key => $eleve)
-                                        <tr>
-                                            <th scope="row">{{ $key + 1 }}</th>
-                                            <td>{{ $eleve->nom  }}</td>
-                                            <td>{{ $eleve->prenom  }}</td>
-                                            <td>{{ \Carbon\Carbon::parse($eleve->updated_at)->format('d/m/Y') }}</td>
-                                            <td class="text-center">
-                                                <a class="btn btn-success" href="{{ route('eleve.show', $eleve) }}">
-                                                    <span class="icon"><i class="fas fa-search"></i></span>
-                                                </a>
-                                                <button type="button" class="btn btn-info" data-toggle="modal"
-                                                        data-target="#editModal{{ $eleve->id }}">
-                                                    <span class="icon"><i class="fas fa-edit"></i></span>
-                                                </button>
-                                                <button type="button" class="btn btn-danger" data-toggle="modal"
-                                                        data-target="#deleteModal{{ $eleve->id }}">
-                                                    <span class="icon"><i class="fas fa-trash-alt"></i></span>
-                                                </button>
-                                            </td>
-                                            <div class="no-height">
-                                                <div class="modal fade" tabindex="-1" role="dialog"
-                                                     id="editModal{{ $eleve->id }}" aria-hidden="true">
-                                                    <div class="modal-dialog modal-lg">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h5><i class="fas fa-users mr-3"></i>Modifier un
-                                                                    élève
-                                                                </h5>
-                                                                <button type="button" class="close"
-                                                                        data-dismiss="modal"
-                                                                        aria-label="Close">
-                                                                    <span aria-hidden="true">&times;</span>
-                                                                </button>
-                                                            </div>
-                                                            @include('eleve.form', ['typeForm'=>"edit", 'model' => $eleve])
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="modal fade" tabindex="-1" role="dialog"
-                                                     id="deleteModal{{ $eleve->id }}" aria-hidden="true">
-                                                    <div class="modal-dialog">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h5><i class="fas fa-users mr-3"></i>Supprimer
-                                                                    l'élève
-                                                                </h5>
-                                                                <button type="button" class="close"
-                                                                        data-dismiss="modal"
-                                                                        aria-label="Close">
-                                                                    <span aria-hidden="true">&times;</span>
-                                                                </button>
-                                                            </div>
-                                                            @include('eleve.form', ['typeForm'=>"delete", 'model' => $eleve])
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                    @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
+                    <div>
+                        <table id="datatable" class="table table-bordered" style="width:100%">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Sexe</th>
+                                <th>Etablissement</th>
+                                <th>Educateur</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($eleves as $key => $eleve)
+                                <tr>
+                                    <td>{{ $eleve->id }}</td>
+                                    <td>{{ $eleve->sexe }}</td>
+                                    <td>{{ $eleve->etablissement->full_name }}</td>
+                                    <td>{{ $eleve->educateur->full_name }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
                     </div>
-
                 </div>
             </div>
         </div>
@@ -118,11 +67,14 @@
 @endsection
 
 @section('javascript')
-    <script>
-        $('#btnExportPDF').click(function (e) {
-            e.preventDefault();
-            console.log("tpotpo")
-        })
-    </script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
 @endsection
 
+@section('jafter')
+    <script>
+        $(document).ready(function () {
+            $('#datatable').DataTable();
+        });
+    </script>
+@endsection
