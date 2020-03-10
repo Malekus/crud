c4294eaada3f2c6c46aaced99e8d6fb5bac08f02
Malekus <lereglissedu92@hotmail.fr>
2020-03-10 08:45:34 +0100

    update button style

--- resources/views/rapport/index.blade.php
+++ resources/views/rapport/index.blade.php
@@ -3,6 +3,8 @@
 @section('endStyle')
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/select/1.3.1/css/select.dataTables.min.css">
+
 @endsection
 
 @section('content')
@@ -15,32 +17,11 @@
                             <h1 class="text-capitalize"><i class="fas fa-file mr-2"></i>rapport</h1>
                         </div>
                     </div>
-                    {{--
-                                            <div class="row">
-                        <div class="col-12">
-                            <div class="float-left">
-                                {!! Form::open(['method' => 'get','url' => route('eleve.index'), 'class' => 'form-inline']) !!}
-                                <div class="form-group mb-2">
-                                    {!! Form::text('search', null, ['class' => 'form-control', 'placeholder' => "Nom de l'élève"]) !!}
-                                </div>
-                                <button type="submit" class="btn btn-info mx-sm-3 mb-2">
-                                    Rechercher
-                                </button>
-                                {!! Form::close() !!}
-                            </div>
-                            <div class="float-left">
-                                <a href="{{ route("rapport.exportPDF", 2) }}" target="_blank" id="btnExportPDF"
-                                   class="btn btn-success mx-sm-3 mb-2">
-                                    Exporter PDF
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    --}}
                     <div>
                         <table id="datatable" class="table table-bordered display" style="width:100%">
                             <thead>
                             <tr>
+                                <th></th>
                                 <th>ID</th>
                                 <th>Nom</th>
                                 <th>Prénom</th>
@@ -52,6 +33,7 @@
                             <tbody>
                             @foreach($eleves as $key => $eleve)
                                 <tr>
+                                    <td></td>
                                     <td>{{ $eleve->id }}</td>
                                     <td>{{ $eleve->nom }}</td>
                                     <td>{{ $eleve->prenom }}</td>
@@ -72,36 +54,38 @@
 @endsection
 
 @section('javascript')
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/select/1.3.1/js/dataTables.select.min.js"></script>
+
 @endsection
 
 @section('jafter')
     <script>
         $(document).ready(function () {
             var table = $('#datatable').DataTable({
-                language : {
-                    "sEmptyTable":     "Aucune donnée disponible dans le tableau",
-                    "sInfo":           "Affichage de l'élément _START_ à _END_ sur _TOTAL_ éléments",
-                    "sInfoEmpty":      "Affichage de l'élément 0 à 0 sur 0 élément",
-                    "sInfoFiltered":   "(filtré à partir de _MAX_ éléments au total)",
-                    "sInfoPostFix":    "",
-                    "sInfoThousands":  ",",
-                    "sLengthMenu":     "Afficher _MENU_ éléments",
+                language: {
+                    "sEmptyTable": "Aucune donnée disponible dans le tableau",
+                    "sInfo": "Affichage de l'élément _START_ à _END_ sur _TOTAL_ éléments",
+                    "sInfoEmpty": "Affichage de l'élément 0 à 0 sur 0 élément",
+                    "sInfoFiltered": "(filtré à partir de _MAX_ éléments au total)",
+                    "sInfoPostFix": "",
+                    "sInfoThousands": ",",
+                    "sLengthMenu": "Afficher _MENU_ éléments",
                     "sLoadingRecords": "Chargement...",
-                    "sProcessing":     "Traitement...",
-                    "sSearch":         "Rechercher :",
-                    "sZeroRecords":    "Aucun élément correspondant trouvé",
+                    "sProcessing": "Traitement...",
+                    "sSearch": "Rechercher :",
+                    "sZeroRecords": "Aucun élément correspondant trouvé",
                     "oPaginate": {
-                        "sFirst":    "Premier",
-                        "sLast":     "Dernier",
-                        "sNext":     "Suivant",
+                        "sFirst": "Premier",
+                        "sLast": "Dernier",
+                        "sNext": "Suivant",
                         "sPrevious": "Précédent"
                     },
                     "oAria": {
-                        "sSortAscending":  ": activer pour trier la colonne par ordre croissant",
+                        "sSortAscending": ": activer pour trier la colonne par ordre croissant",
                         "sSortDescending": ": activer pour trier la colonne par ordre décroissant"
                     },
                     "select": {
@@ -112,23 +96,30 @@
                         }
                     }
                 },
-                dom: "<'row'<'col-sm-12 col-md-6'lB><'col-sm-12 col-md-6'f>>" +
-                    "<'row'<'col-sm-12'tr>>" +
-                    "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
-
+                dom:    "<'row'<'col-sm-12 col-md-6'<'d-inline-block'l><'d-inline-block ml-3'B>><'col-sm-12 col-md-6'f>>" +
+                        "<'row'<'col-sm-12'tr>>" +
+                        "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
                 buttons: [
                     {
-                        text: 'Exporter',
+                        text: '<i class="fas fa-download"></i> Exporter',
                         className: 'btn btn-primary',
-                        action: function ( e, dt, node, config ) {
-                            alert( 'Button activated' );
+                        action: function (e, dt, node, config) {
+                            alert('Button activated');
                         }
                     }
-                ]
+                ],
+                columnDefs: [{
+                    orderable: false,
+                    className: 'select-checkbox',
+                    targets: 0,
+                    checkboxes: true
+                }],
+                select: {
+                    style: 'multi',
+                    selector: 'td:first-child'
+                },
+                order: [[1, 'asc']]
             });
         });
-
-        // ml-3 d-inline-block
-
     </script>
 @endsection
